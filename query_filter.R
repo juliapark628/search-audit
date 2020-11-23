794d2196b59fd7761b993845d1ea1b53dd03a41a
Julia Park <julpark@stanford.edu>
2020-11-23 14:58:27 -0800

    Added filtered queries

---
+++ query_filter.R
@@ -0,0 +1,36 @@
+# Description
+# Deletes all queries with [state] and [city]
+
+# Author: Julia Park
+# Version: 2020-11-23
+
+# Libraries
+library(tidyverse)
+library(readxl)
+
+
+# Parameters
+setwd("~/GitHub/Lab_Legal_Design/search_audit")
+
+file_eviction <- "eviction"
+file_debt_collection <- "debt_collection"
+file_flood_contractor <- "flood_contractor"
+file_domestic_violence <- "domestic_violence"
+
+
+
+
+#===============================================================================
+
+clean_csv_file <- function(type) {
+  str_glue(type, ".csv") %>% 
+    read_csv(col_names = "query") %>% 
+    filter(!str_detect(query, "\\[state\\]")) %>% 
+    filter(!str_detect(query, "\\[city\\]")) %>% 
+    write_csv(str_glue(type, "_filtered.csv"), col_names = FALSE)
+}
+
+clean_csv_file(file_eviction)
+clean_csv_file(file_debt_collection)
+clean_csv_file(file_flood_contractor)
+clean_csv_file(file_domestic_violence)
